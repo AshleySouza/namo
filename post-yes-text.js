f780b0d3c85cd2d29c41d7843c41318576d0d3d7
Ashley Souza <AshleySouza@users.noreply.github.com>
2026-08-19 21:57:35 -0300

    Ajustar mensagem final

--- post-yes-text.js
+++ post-yes-text.js
@@ -6,4 +6,4 @@ celebrationParagraphs[0].textContent = 'Agora podemos continuar escrevendo a nos
 celebrationParagraphs[1].textContent = 'Você é a mulher mais linda, cheirosa e incrível que eu poderia ter encontrado.';
 celebrationFinal.innerHTML = `<p>Obrigada por escolher continuar.<br>E obrigada por ser você.</p>
   <p class="celebration-names">Ashley ♥ Samira</p>
-  <div class="final-simple"><p>Samira.</p><p>Eu não sei o que o futuro reserva.<br>Mas sei que quero descobrir com você.</p><h2>Eu te amo muito.<br>Minha mulher. <i>♥</i></h2><small>Ashley</small><small>28/03/2026 → ∞</small></div>`;
+  <div class="final-simple"><p>Samira.</p><p>Eu não sei o que o futuro reserva.<br>Mas sei que quero descobrir com você.</p><h2>Eu te amo muito.<br>Quero viver muitos momentos ao seu lado. <i>♥</i></h2><small>Ashley</small><small>28/03/2026 → ∞</small></div>`;
